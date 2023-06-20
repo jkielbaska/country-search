af63bfd6069a745fec234ab675ea03f2c3ef9951
Jakub <kielbaskakuba@gmail.com>
2023-06-20 19:21:46 +0200

    Add latlng from redux store to marker and center

--- src/components/Right/Map.tsx
+++ src/components/Right/Map.tsx
@@ -1,34 +1,33 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import L, { LatLngExpression } from "leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import { selectorLatlng } from "../../redux/slices/latlngSlice";
+import { useAppSelector } from "../../redux/hooks";
+import { FlyToCapital } from "./FlyToCapital";
 
 const icon = L.icon({
   iconUrl: "/pin-icon.png",
   iconSize: [25, 41],
 });
 
-export const Map = ({
-  latlng = [51.505, -0.09],
-}: {
-  latlng: LatLngExpression;
-}) => {
+export const Map = () => {
+  const latlng = useAppSelector(selectorLatlng);
+
   return (
     <div className="w-[70%] bg-slate-400 h-screen">
       <MapContainer
         style={{ height: "100%", width: "100%" }}
         center={latlng}
-        zoom={10}
-        scrollWheelZoom={false}
+        zoom={5}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={latlng} icon={icon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker position={latlng} icon={icon}></Marker>
+
+        <FlyToCapital />
       </MapContainer>
     </div>
   );
