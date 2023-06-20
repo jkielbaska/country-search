ad2655531a8b838eccc62ffe6529e416891a2a8b
Jakub <kielbaskakuba@gmail.com>
2023-06-21 01:43:01 +0200

    Delete unnecessary async/await

--- src/hooks/useCountries.ts
+++ src/hooks/useCountries.ts
@@ -1,45 +1,61 @@
 import { useEffect, useState } from "react";
 import { CountriesData } from "../types/tCountriesData";
-import { getAllCountries, getCountryByName } from "../services/api";
+import {
+  getAllCountries,
+  getCountryByName,
+  getCountryByCapital,
+} from "../services/api";
 import { useAppSelector } from "../redux/hooks";
 import { selectorSearch } from "../redux/slices/searchSlice";
-import { AxiosError } from "axios";
 
 export const useCountries = () => {
   const [countries, setCountries] = useState<CountriesData[]>([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState<string>("");
 
   const search = useAppSelector(selectorSearch);
 
   useEffect(() => {
-    const fetchCountries = async function fetchCountries() {
+    const fetchCountries = async () => {
       try {
         setLoading(true);
         setError("");
 
         if (search === "") {
           const response = await getAllCountries();
-          response && setCountries(response);
+          response && setCountries([...response]);
         } else {
-          const response = search && (await getCountryByName(search));
-          response && setCountries(response);
+          const byName =
+            search && (await getCountryByName(search).catch(() => []));
+          const byCapital =
+            search && (await getCountryByCapital(search).catch(() => []));
+
+          const mergedResponse = [...(byName ?? []), ...(byCapital ?? [])];
+          const filteredResponse = mergedResponse.reduce<CountriesData[]>(
+            (acc, country) => {
+              const existingCountry = acc.find(
+                (c) => c?.cca2 === (country as CountriesData)?.cca2
+              );
+              if (!existingCountry) {
+                acc.push(country as CountriesData);
+              }
+              return acc;
+            },
+            [] as CountriesData[]
+          );
+
+          filteredResponse.length === 0
+            ? setError("No country or capital found, try again")
+            : setCountries(filteredResponse);
         }
       } catch (error: unknown) {
         console.error(error);
-        if (
-          error instanceof AxiosError &&
-          error.response &&
-          error.response.status === 404
-        ) {
-          setError("No country found, try another one");
-        } else {
-          setError("An error occurred. Please try again.");
-        }
+        setError("An error occurred. Please try again.");
       } finally {
         setLoading(false);
       }
     };
+
     fetchCountries();
   }, [search]);
 
