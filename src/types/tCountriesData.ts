cec825348cdd4c774b1e576587127c0bae03e81a
Jakub <kielbaskakuba@gmail.com>
2023-06-20 12:47:26 +0200

    Add typing for api response

---
+++ src/types/tCountriesData.ts
@@ -0,0 +1,42 @@
+import { LatLngExpression } from "leaflet";
+
+export interface CountriesData {
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  capital: string[];
+  latlng: LatLngExpression;
+  demonyms: {
+    [key: string]: {
+      f: string;
+      m: string;
+    };
+  };
+  flag: string;
+  maps: {
+    googleMaps: string;
+    openStreetMaps: string;
+  };
+  population: number;
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  capitalInfo: {
+    latlng: LatLngExpression;
+  };
+}
