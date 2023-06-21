6e3d3907980925fb626360a7ef97dcd64b1632cc
Jakub <kielbaskakuba@gmail.com>
2023-06-21 18:24:43 +0200

    Change initialLoad to show whole world and to not display marker. Smaller pin icon

--- src/components/Right/Map.tsx
+++ src/components/Right/Map.tsx
@@ -1,31 +1,32 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import L from "leaflet";
+import { initialLatLng } from "../../redux/slices/latlngSlice";
 import { selectorLatlng } from "../../redux/slices/latlngSlice";
 import { useAppSelector } from "../../redux/hooks";
 import { FlyToCapital } from "./FlyToCapital";
 
 const icon = L.icon({
   iconUrl: "/pin-icon.png",
-  iconSize: [25, 41],
+  iconSize: [20, 35],
 });
 
 export const Map = () => {
   const latlng = useAppSelector(selectorLatlng);
 
   return (
-    <div className="w-[70%] bg-slate-400 h-screen">
+    <div className="md:w-[70%] w-full bg-slate-400 h-screen">
       <MapContainer
         style={{ height: "100%", width: "100%" }}
         center={latlng}
-        zoom={5}
+        zoom={2}
         scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={latlng} icon={icon}></Marker>
+        {latlng !== initialLatLng && <Marker position={latlng} icon={icon} />}
 
         <FlyToCapital />
       </MapContainer>
