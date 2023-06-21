2d5bb7f8f922174ffa25191de4419af8e9131c06
Jakub <kielbaskakuba@gmail.com>
2023-06-21 18:23:22 +0200

    Add condition using latlng to zoom map always the same when setView is run

--- src/components/Right/FlyToCapital.tsx
+++ src/components/Right/FlyToCapital.tsx
@@ -3,13 +3,17 @@ import { useMap } from "react-leaflet";
 import { selectorLatlng } from "../../redux/slices/latlngSlice";
 import { useAppSelector } from "../../redux/hooks";
 import { useEffect } from "react";
+import { initialLatLng } from "../../redux/slices/latlngSlice";
 
 export const FlyToCapital = () => {
   const latlng = useAppSelector(selectorLatlng);
   const map = useMap();
 
   useEffect(() => {
-    map.setView(L.latLng(latlng), map.getZoom(), { animate: true });
+    latlng !== initialLatLng &&
+      map.setView(L.latLng(latlng), 6.5, {
+        animate: true,
+      });
   }, [latlng, map]);
 
   return null;
