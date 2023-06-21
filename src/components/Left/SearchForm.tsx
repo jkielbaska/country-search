a91ad4b0fd2d8ef1ec6398b6020f2ddbb53c793b
Jakub <kielbaskakuba@gmail.com>
2023-06-21 23:25:44 +0200

    Add select to choose searchType. Disable input and button when searchType not selected, improve placeholders

--- src/components/Left/SearchForm.tsx
+++ src/components/Left/SearchForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSearchCountries } from "../../hooks/useSearchCountries";
 
 export const SearchForm = () => {
@@ -5,17 +6,45 @@ export const SearchForm = () => {
     form: { handleAddSearch, register },
   } = useSearchCountries();
 
+  const [selectedOption, setSelectedOption] = useState<string>("");
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <form onSubmit={handleAddSearch}>
       <input
         type="search"
-        placeholder="SEARCH COUNTRY OR CAPITAL"
-        className="input text-center w-full"
+        placeholder={
+          selectedOption === ""
+            ? "SELECT FILTERING TYPE FIRST"
+            : `SEARCH ${selectedOption.toUpperCase()}`
+        }
+        className={`input text-center w-full ${
+          selectedOption === "" && "cursor-not-allowed"
+        }`}
         {...register("search")}
+        disabled={selectedOption === ""}
       />
-
-      <button type="submit" className="btn btn-active w-full ">
-        find
+      <select
+        className="select w-full"
+        {...register("type")}
+        onChange={handleOptionChange}
+        defaultValue=""
+      >
+        <option disabled value="">
+          CHOOSE FILTERING TYPE
+        </option>
+        <option value="country">COUNTRY</option>
+        <option value="capital">CAPITAL</option>
+        <option value="both">BOTH</option>
+      </select>
+      <button
+        type="submit"
+        className="btn btn-active w-full"
+        disabled={selectedOption === ""}
+      >
+        {selectedOption === "" ? "SELECT FILTERING TYPE FIRST" : "FIND"}
       </button>
     </form>
   );
