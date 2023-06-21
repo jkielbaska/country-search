f222ebe77b4c685706c1fa721f2f2739c07ffda3
Jakub <kielbaskakuba@gmail.com>
2023-06-21 23:22:59 +0200

    Add searchType reducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchSlice from "./slices/searchSlice";
 import latlngSlice from "./slices/latlngSlice";
+import searchTypeSlice from "./slices/searchTypeSlice";
 
 export const store = configureStore({
   reducer: {
     search: searchSlice.reducer,
     latlng: latlngSlice.reducer,
+    searchType: searchTypeSlice.reducer,
   },
 });
 
