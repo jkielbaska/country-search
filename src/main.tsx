d5a0f6321b4e8d2acbec6af910f3c57bb506a129
Jakub <kielbaskakuba@gmail.com>
2023-06-21 18:18:33 +0200

    Change index.css naming for global.css to be more correct with tailwind docs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./global.css";
 import App from "./App.tsx";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
